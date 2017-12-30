e568aa7364b80c0da7e4ad91dd1561f6a5861174
ApolloBear <yxmbilka@gmail.com>
2017-12-30 23:42:32 +0300

    задание 14 доработка 19

--- js/pictures.js
+++ js/pictures.js
@@ -233,31 +233,38 @@ var buttonInc = document.querySelector('.upload-resize-controls-button-inc').add
 });
 
 var checkForm = function() {
-      var isFormValid = true;
-      var inputHashtag = document.querySelector('.upload-form-hashtags');
-      var hashtags = inputHashtag.value;
-      console.log(hashtags);
-      var hashtagList = hashtags.toLowerCase().split(' ');
-      console.log(hashtagList);
+  var isFormValid = true;
+  var inputHashtag = document.querySelector('.upload-form-hashtags');
+  var hashtags = inputHashtag.value;
+  console.log(hashtags);
+  var hashtagList = hashtags.toLowerCase().split(' ');
+  console.log(hashtagList);
 
-      if (hashtagList.length > 5) inputHashtag.setCustomValidity('Не может быть больше 5 хэш-тегов');
+  if (hashtagList.length > 5) {
+    inputHashtag.setCustomValidity('Не может быть больше 5 хэш-тегов');
+    inputHashtag.reportValidity();
+    return false;
+  }
 
-      for (var i = 0; i < hashtagList.length; i++) {
-        if (hashtagList[i].length > 20) {
-          inputHashtag.setCustomValidity('Хэш-тег не может быть более 20 символов');
-          isFormValid = false;
-          break;
-        } else if (hashtagList[i].charAt(0) !== '#') {
-          inputHashtag.setCustomValidity('Хэш-тег должен начинаться с #');
-          isFormValid = false;
-          break;
-        } else if (hashtagList.indexOf(hashtags[i]) !== i) {
-          inputHashtag.setCustomValidity('Хэш-тег не может повторяться');
-          isFormValid = false;
-          break;
-        } else {
-          inputHashtag.setCustomValidity('');
-        }
-      }
-      return isFormValid;
+  for (var i = 0; i < hashtagList.length; i++) {
+    if (hashtagList[i].length > 20) {
+      inputHashtag.setCustomValidity('Хэш-тег не может быть более 20 символов');
+      inputHashtag.reportValidity();
+      isFormValid = false;
+      break;
+    } else if (hashtagList[i].charAt(0) !== '#') {
+      inputHashtag.setCustomValidity('Хэш-тег должен начинаться с #');
+      inputHashtag.reportValidity();
+      isFormValid = false;
+      break;
+    } else if (hashtagList.indexOf(hashtagList[i]) !== i) {
+      inputHashtag.setCustomValidity('Хэш-тег не может повторяться');
+      inputHashtag.reportValidity();
+      isFormValid = false;
+      break;
+    } else {
+      inputHashtag.setCustomValidity('');
     }
+  }
+  return isFormValid;
+}
