df058a946a275bbc4f8d5d34528904230de2c0b0
ApolloBear <yxmbilka@gmail.com>
2018-01-18 17:52:00 +0300

    задание 14 доработка 23

--- js/pictures.js
+++ js/pictures.js
@@ -1,58 +1,58 @@
 'use strict';
-var photoObjects = [];
-var objectComments = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
+// var photoObjects = [];
+// var objectComments = [
+//   'Всё отлично!',
+//   'В целом всё неплохо. Но не всё.',
+//   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+//   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+//   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+//   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+// ];
 
-var objectLikes = getRandomint(15, 200);
+// var objectLikes = getRandomint(15, 200);
 
-var randomComment = objectComments[Math.floor(Math.random() * objectComments.length)];
+// var randomComment = objectComments[Math.floor(Math.random() * objectComments.length)];
 
-var KEYCODE_ESC = 27;
-var KEYCODE_ENTER = 13;
+// var KEYCODE_ESC = 27;
+// var KEYCODE_ENTER = 13;
 
 var popUpClose = document.querySelector('.gallery-overlay-close');
 popUpClose.addEventListener('click', function() {
   document.querySelector('.gallery-overlay').classList.add('hidden');
 });
 
-for (var i = 0; i <= 24; i++) {
-  photoObjects[i] = {
-    url: 'photos/' + (i + 1) + '.jpg',
-    likes: objectLikes,
-    comments: randomComment,
-  };
-  document.querySelector('#picture-template .picture img');
-  document.querySelector('#picture-template .picture-likes');
-  document.querySelector('#picture-template .picture-comments');
-}
+// for (var i = 0; i <= 24; i++) {
+//   photoObjects[i] = {
+//     url: 'photos/' + (i + 1) + '.jpg',
+//     likes: objectLikes,
+//     comments: randomComment,
+//   };
+//   document.querySelector('#picture-template .picture img');
+//   document.querySelector('#picture-template .picture-likes');
+//   document.querySelector('#picture-template .picture-comments');
+// }
 
-var renderPhoto = function(photoObject) {
-  var clonePictureTemplate = originalPictureTemplate.cloneNode(true).content;
-  clonePictureTemplate.querySelector('.picture img').setAttribute('src', photoObject.url);
-  clonePictureTemplate.querySelector('.picture-likes').textContent = photoObject.likes;
-  clonePictureTemplate.querySelector('.picture-comments').textContent = photoObject.comments;
-  return clonePictureTemplate;
-};
+// var renderPhoto = function(photoObject) {
+//   var clonePictureTemplate = originalPictureTemplate.cloneNode(true).content;
+//   clonePictureTemplate.querySelector('.picture img').setAttribute('src', photoObject.url);
+//   clonePictureTemplate.querySelector('.picture-likes').textContent = photoObject.likes;
+//   clonePictureTemplate.querySelector('.picture-comments').textContent = photoObject.comments;
+//   return clonePictureTemplate;
+// };
 
-var originalPictureTemplate = document.querySelector('#picture-template');
+// var originalPictureTemplate = document.querySelector('#picture-template');
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < 25; i++) {
-  fragment.appendChild(renderPhoto(photoObjects[i]));
-}
+// var fragment = document.createDocumentFragment();
+// for (i = 0; i < 25; i++) {
+//   fragment.appendChild(renderPhoto(photoObjects[i]));
+// }
 
-var pageListPhoto = document.querySelector('.pictures');
-pageListPhoto.appendChild(fragment);
+// var pageListPhoto = document.querySelector('.pictures');
+// pageListPhoto.appendChild(fragment);
 
-function getRandomint() {
-  return Math.floor(Math.random() * (200 - 15)) + 15;
-}
+// function getRandomint() {
+//   return Math.floor(Math.random() * (200 - 15)) + 15;
+// }
 
 var popUpImage = document.querySelector('.gallery-overlay-image');
 popUpImage.setAttribute('src', photoObjects[0].url);
@@ -86,68 +86,68 @@ picturePage.forEach(function(picture) {
   picture.addEventListener('click', pictureClickHandler);
 });
 
-var keyboardOpen = document.querySelector('.picture');
-keyboardOpen.addEventListener('keydown', function(event) {
-  event.preventDefault();
-  if (event.keyCode === KEYCODE_ENTER) {
-    document.querySelector('.gallery-overlay').classList.remove('hidden');
-  }
-});
+// var keyboardOpen = document.querySelector('.picture');
+// keyboardOpen.addEventListener('keydown', function(event) {
+//   event.preventDefault();
+//   if (event.keyCode === KEYCODE_ENTER) {
+//     document.querySelector('.gallery-overlay').classList.remove('hidden');
+//   }
+// });
+//
+// document.querySelector('.gallery-overlay-close').addEventListener('keydown', function(event) {
+//   event.preventDefault();
+//   if (event.keyCode === KEYCODE_ENTER) {
+//     document.querySelector('.gallery-overlay').classList.add('hidden');
+//   }
+// });
+//
+// document.addEventListener('keydown', function(event) {
+//   if (event.keyCode === KEYCODE_ESC) {
+//     document.querySelector('.gallery-overlay').classList.add('hidden');
+//   }
+// });
 
-document.querySelector('.gallery-overlay-close').addEventListener('keydown', function(event) {
-  event.preventDefault();
-  if (event.keyCode === KEYCODE_ENTER) {
-    document.querySelector('.gallery-overlay').classList.add('hidden');
-  }
-});
+// var uploadImage = document.querySelector('.upload-overlay');
 
-document.addEventListener('keydown', function(event) {
-  if (event.keyCode === KEYCODE_ESC) {
-    document.querySelector('.gallery-overlay').classList.add('hidden');
-  }
-});
+// document.querySelector('#upload-select-image').addEventListener('change', function() {
+//   if (document.activeElement.className !== 'upload-form-description') {
+//     uploadImage.classList.remove('hidden');
+//   }
+// });
 
-var uploadImage = document.querySelector('.upload-overlay');
+// document.querySelector('.upload-form-cancel').addEventListener('click', function() {
+//   uploadImage.classList.add('hidden');
+// });
 
-document.querySelector('#upload-select-image').addEventListener('change', function() {
-  if (document.activeElement.className !== 'upload-form-description') {
-    uploadImage.classList.remove('hidden');
-  }
-});
+// document.addEventListener('keydown', function(event) {
+//   if (event.keyCode === KEYCODE_ESC) {
+//     uploadImage.classList.add('hidden');
+//   }
+// });
 
-document.querySelector('.upload-form-cancel').addEventListener('click', function() {
-  uploadImage.classList.add('hidden');
-});
+// document.querySelector('.upload-form-cancel').addEventListener('keydown', function(event) {
+//   event.preventDefault();
+//   if (event.keyCode === KEYCODE_ENTER) {
+//     uploadImage.classList.add('hidden');
+//   }
+// });
 
-document.addEventListener('keydown', function(event) {
-  if (event.keyCode === KEYCODE_ESC) {
-    uploadImage.classList.add('hidden');
-  }
-});
-
-document.querySelector('.upload-form-cancel').addEventListener('keydown', function(event) {
-  event.preventDefault();
-  if (event.keyCode === KEYCODE_ENTER) {
-    uploadImage.classList.add('hidden');
-  }
-});
-
-var buttonSubmit = document.querySelector('.upload-form-submit');
-
-buttonSubmit.addEventListener('click', function() {
-  if (checkForm()) {
-    document.querySelector('#upload-select-image').submit();
-  }
-});
-
-buttonSubmit.addEventListener('click', function() {
-  event.preventDefault();
-  if (event.keyCode === KEYCODE_ENTER) {
-    if (checkForm()) {
-      document.querySelector('#upload-select-image').submit();
-    }
-  }
-});
+// var buttonSubmit = document.querySelector('.upload-form-submit');
+//
+// buttonSubmit.addEventListener('click', function() {
+//   if (checkForm()) {
+//     document.querySelector('#upload-select-image').submit();
+//   }
+// });
+//
+// buttonSubmit.addEventListener('click', function() {
+//   event.preventDefault();
+//   if (event.keyCode === KEYCODE_ENTER) {
+//     if (checkForm()) {
+//       document.querySelector('#upload-select-image').submit();
+//     }
+//   }
+// });
 
 document.querySelector('.upload-effect-controls').addEventListener('click', function(event) {
   var value = event.target.value;
@@ -237,39 +237,39 @@ var buttonInc = document.querySelector('.upload-resize-controls-button-inc').add
   }
 });
 
-var checkForm = function() {
-  var isFormValid = true;
-  var inputHashtag = document.querySelector('.upload-form-hashtags');
-  var hashtags = inputHashtag.value;
-  console.log(hashtags);
-  var hashtagList = hashtags.toLowerCase().split(' ');
-  console.log(hashtagList);
-
-  if (hashtagList.length > 5) {
-    inputHashtag.setCustomValidity('Не может быть больше 5 хэш-тегов');
-    inputHashtag.reportValidity();
-    return false;
-  }
-
-  for (var i = 0; i < hashtagList.length; i++) {
-    if (hashtagList[i].length > 20) {
-      inputHashtag.setCustomValidity('Хэш-тег не может быть более 20 символов');
-      inputHashtag.reportValidity();
-      isFormValid = false;
-      break;
-    } else if (hashtagList[i].charAt(0) !== '#') {
-      inputHashtag.setCustomValidity('Хэш-тег должен начинаться с #');
-      inputHashtag.reportValidity();
-      isFormValid = false;
-      break;
-    } else if (hashtagList.indexOf(hashtagList[i]) !== i) {
-      inputHashtag.setCustomValidity('Хэш-тег не может повторяться');
-      inputHashtag.reportValidity();
-      isFormValid = false;
-      break;
-    } else {
-      inputHashtag.setCustomValidity('');
-    }
-  }
-  return isFormValid;
-};
+// var checkForm = function() {
+//   var isFormValid = true;
+//   var inputHashtag = document.querySelector('.upload-form-hashtags');
+//   var hashtags = inputHashtag.value;
+//   console.log(hashtags);
+//   var hashtagList = hashtags.toLowerCase().split(' ');
+//   console.log(hashtagList);
+//
+//   if (hashtagList.length > 5) {
+//     inputHashtag.setCustomValidity('Не может быть больше 5 хэш-тегов');
+//     inputHashtag.reportValidity();
+//     return false;
+//   }
+//
+//   for (var i = 0; i < hashtagList.length; i++) {
+//     if (hashtagList[i].length > 20) {
+//       inputHashtag.setCustomValidity('Хэш-тег не может быть более 20 символов');
+//       inputHashtag.reportValidity();
+//       isFormValid = false;
+//       break;
+//     } else if (hashtagList[i].charAt(0) !== '#') {
+//       inputHashtag.setCustomValidity('Хэш-тег должен начинаться с #');
+//       inputHashtag.reportValidity();
+//       isFormValid = false;
+//       break;
+//     } else if (hashtagList.indexOf(hashtagList[i]) !== i) {
+//       inputHashtag.setCustomValidity('Хэш-тег не может повторяться');
+//       inputHashtag.reportValidity();
+//       isFormValid = false;
+//       break;
+//     } else {
+//       inputHashtag.setCustomValidity('');
+//     }
+//   }
+//   return isFormValid;
+// };
